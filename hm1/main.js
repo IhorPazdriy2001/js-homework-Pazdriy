69fbd9c850a5e267587073be4c6fe59170cb4807
IhorPazdriy2001 <gogearmuse@gmail.com>
2022-02-01 19:55:21 +0200

    1hm

---
+++ hm1/main.js
@@ -0,0 +1,173 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let greeting = 'hello';
+// console.log(greeting);
+// alert(greeting);
+// document.write("<h4>hello</h4>")
+//
+// let owu = 'owu';
+// console.log(owu);
+// alert(owu);
+// document.write("<h4>owu</h4>")
+//
+// let com = "com";
+// console.log(com);
+// alert(com);
+// document.write("<h4>com</h4>")
+//
+// let ua = "ua";
+// console.log(ua);
+// alert(ua);
+// document.write("<h4>ua</h4>");
+//
+// let num1 = 1;
+// console.log(num1);
+// alert(num1);
+// document.write("<h4>1</h4>");
+//
+// let num2 = 10;
+// console.log(num2);
+// alert(num2);
+// document.write("<h4>10</h4>");
+//
+// let num3 = -999;
+// console.log(num3);
+// alert(num3);
+// document.write("<h4>-999</h4>");
+//
+// let num4 = 123;
+// console.log(num4);
+// alert(num4);
+// document.write("<h4>123</h4>");
+//
+// const num5 = 3.14;
+// console.log(num5);
+// alert(num5);
+// document.write("<h4>π = 3.14</h4>");
+//
+// let num6 = 2.7;
+// console.log(num6);
+// alert(num6);
+// document.write("<h4>2.7</h4>");
+//
+// let num7 = 16;
+// console.log(num7);
+// alert(num7);
+// document.write("<h4>16</h4>");
+//
+// let True = true;
+// console.log(True);
+// alert(True);
+// document.write("<h4>True</h4>")
+//
+// let False = false;
+// console.log(False);
+// alert(False);
+// document.write("<h4>False</h4>")
+
+// Переприсвоїти кожній змінній з завдання значення на довільне.
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let world = 'world';
+// console.log(world);
+// alert(world);
+// document.write("<h4>world</h4>")
+//
+// let phrase = 'me';
+// console.log(phrase);
+// alert(phrase);
+// document.write("<h4>me</h4>")
+//
+// let net = "net";
+// console.log(net);
+// alert(net);
+// document.write("<h4>net</h4>")
+//
+// let co = "co";
+// console.log(co);
+// alert(co);
+// document.write("<h4>co</h4>");
+//
+// let num_1 = 3;
+// console.log(num_1);
+// alert(num_1);
+// document.write("<h4>3</h4>");
+//
+// let num_2 = 15;
+// console.log(num_2);
+// alert(num_2);
+// document.write("<h4>15</h4>");
+//
+// let num_3 = 5 - 20;
+// console.log(num_3);
+// alert(num_3);
+// document.write("<h4>-15</h4>");
+//
+// let num_4 = 321;
+// console.log(num_4);
+// alert(num_4);
+// document.write("<h4>321</h4>");
+//
+// const num_5 = 1.618;
+// console.log(num_5);
+// alert(num_5);
+// document.write("<h4>φ = 1.618</h4>");
+//
+// let num_6 = 7.7;
+// console.log(num_6);
+// alert(num_6);
+// document.write("<h4>7.7</h4>");
+//
+// let num_7 = 165;
+// console.log(num_7);
+// alert(num_7);
+// document.write("<h4>165</h4>");
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let firstName = "Ihor ";
+// let lastName = "Pazdriy";
+// let middleName = "Romanovych ";
+// let person = firstName + middleName + lastName;
+// console.log(person);
+
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// let name = prompt('Write you name: ', '');
+// let midName = prompt('Write you middle name: ', "");
+// let year = prompt("How old are you? ", "")
+// console.log('Hello, ' + name + ' ' + midName + ', You are ' + year + ' years old ');
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// console.log(typeof(a));
+// let b = "100";
+// console.log(typeof(b));
+// let c = true;
+// console.log(typeof (c));
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
+// 5 ? 6 -> true                    5 < 6
+// 5 ? 6 -> false                   5 > 6
+// 5 ? 6 -> false                   5 >= 6
+// 5 ? 6 -> false                   5 == 6
+// 10 ? 10 -> true                  10 == 10
+// 10 ? 10 -> true                  10 >= 10
+// 10 ? 10 -> false                 10 < 10
+// 10 ? 10 -> false                 10 > 10
+// 10 ? 10 -> false                 10 != 10
+// 123 ? '123' -> false             123 > 123
+// 123 ? '123' -> true              123 !== 123
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); 205 + в даному випадку не математична дія того виходить string + string
+// document.write(str - a + "<br/>"); 15  str перетворюється на int 20-5=15.
+// document.write(str * "2" + "<br/>"); 40 str перетворюється на int
+// document.write(str / 2 + "<br/>"); 10 str перетворюється на int
+
+
+
