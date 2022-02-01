bc6ae692f2c2071c771ff492b8283658d238c83c
IhorPazdriy2001 <gogearmuse@gmail.com>
2022-02-01 22:50:06 +0200

    cw done

---
+++ cw1/main.js
@@ -0,0 +1,59 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let arr = [1,10,5,16,7,45,76,8,45,3];
+// let result = arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]+arr[9];
+// console.log(result)
+//
+//
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+/* let book = {
+    name: "Stepan_Bandera",
+    number_of_pages: 350,
+    genre: "history"
+}
+console.log(book) */
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+
+// let book = {
+//     name: "Stepan_Bandera",
+//     number_of_pages: 350,
+//     genre: "history",
+//     author: "Yanevsky D."
+// }
+// console.log(book)
+
+// 4. Створити маис об'єктів book з наступними полями
+// - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+
+// let arr_books = {
+//     book1: [{name: "Stepan Bandera", number_of_pages: 350, genre: "history", author: "Yanevsky D."}],
+//     book2: [{name: "Taras Bulba", number_of_pages: 320, genre: "Historical novel", author: "Mykola Gogol"}]
+// }
+// console.log(arr_books.book1,arr_books.book2)
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+// let heigt = 23;
+// let width = 10;
+// let s = heigt * width;
+// console.log(s);
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+/*let heightC = 10;
+let dc = 4;
+const pi = 3.14;
+let r = dc/2;
+let v = pi * r * heightC;
+console.log(Math.trunc(v));*/
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора
+// (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+//
+// let n = 3;
+// let m = 4;
+// let k = Math.pow(n,2) + Math.pow(m,2)
+// console.log(Math.sqrt(k))
